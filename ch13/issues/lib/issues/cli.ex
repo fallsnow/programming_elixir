1ef0230a18d9b03dcbf45729e7f37ab548ae14f2
takoshima <fallsnow@gmail.com>
2018-04-11 02:10:19 +0000

    Task: Make a Command-Line Executable

--- ch13/issues/lib/issues/cli.ex
+++ ch13/issues/lib/issues/cli.ex
@@ -1,4 +1,5 @@
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [print_table_for_columns: 2]
 
   @default_count 4
 
@@ -8,7 +9,7 @@ defmodule Issues.CLI do
   table of the last _n_ issues in a github project
   """
 
-  def run(argv) do
+  def main(argv) do
     argv
     |> parse_args
     |> process
@@ -24,30 +25,33 @@ defmodule Issues.CLI do
   """
 
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, switches: [ help: :boolean ],
-                                     aliases:  [ h:    :help    ])
+    parse =
+      OptionParser.parse(
+        argv,
+        switches: [help: :boolean],
+        aliases: [h: :help]
+      )
 
     case parse do
+      {[help: true], _, _} ->
+        :help
 
-    { [ help: true ], _, _ }
-      -> :help
+      {_, [user, project, count], _} ->
+        {user, project, String.to_integer(count)}
 
-    { _, [ user, project, count ], _ }
-      -> { user, project, 
-           String.to_integer(count) }
+      {_, [user, project], _} ->
+        {user, project, @default_count}
 
-    { _, [ user, project ], _ }
-      -> { user, project, @default_count }
-
-    _ -> :help
-
+      _ ->
+        :help
     end
   end
 
   def process(:help) do
-    IO.puts """
+    IO.puts("""
     usage: issues <user> <project> [ count | #{@default_count} ]
-    """
+    """)
+
     System.halt(0)
   end
 
@@ -57,32 +61,23 @@ defmodule Issues.CLI do
     |> convert_to_list_of_maps
     |> sort_into_ascending_order
     |> Enum.take(count)
-    |> show_in_tabular
-    #|> show_in_tabular
-    #|> Enum.find(fn(x) -> x["number"] == 6768 end)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}), do: body
 
   def decode_response({:eror, error}) do
     {_, message} = List.keyfind(error, "message", 0)
-    IO.puts "Error fetching from Github: #{message}"
+    IO.puts("Error fetching from Github: #{message}")
     System.halt(2)
   end
 
   def convert_to_list_of_maps(list) do
     list
-    |> Enum.map(&Enum.into(&1, Map.new))
+    |> Enum.map(&Enum.into(&1, Map.new()))
   end
 
   def sort_into_ascending_order(list_of_issues) do
-    Enum.sort list_of_issues, fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
-  end
-
-  def show_in_tabular(issues) do
-    IO.puts "#     | Created at        | Title"
-    IO.puts "------+-------------------+---------------------------------------"
-    issues
-    |> Enum.map(fn(x) -> IO.puts "#{x["number"]} | #{x["created_at"]} | #{x["title"]}" end)
+    Enum.sort(list_of_issues, fn i1, i2 -> i1["created_at"] <= i2["created_at"] end)
   end
 end
--- ch13/issues/lib/issues/github_issues.ex
+++ ch13/issues/lib/issues/github_issues.ex
@@ -1,12 +1,12 @@
 defmodule Issues.GithubIssues do
-  @user_agent [ {"User-agent", "Elixir dave@pragprog.com"} ]
+  @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
   # use a module attribute to fetch the value at compile time
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
-    issues_url(user, project)
     # |> HTTPoison.get(@user_agent)
     # Add an option to get through a proxy
+    issues_url(user, project)
     |> HTTPoison.get(@user_agent, [{:proxy, "http://133.192.24.101:8080"}])
     |> handle_response
   end
@@ -16,13 +16,13 @@ defmodule Issues.GithubIssues do
   end
 
   # def handle_response({ :ok, %{status_code: 200, body: body}}) do
-  def handle_response({ :ok, %HTTPoison.Response{status_code: 200, body: body}}) do
-    { :ok, Poison.Parser.parse!(body) }
+  def handle_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+    {:ok, Poison.Parser.parse!(body)}
   end
 
   # def handle_response({ _, %{status_code: _, body: body}}) do
   #  { :error, body}
-  def handle_response({ _, %HTTPoison.Error{reason: reason}}) do
-    { :error, reason }
+  def handle_response({_, %HTTPoison.Error{reason: reason}}) do
+    {:error, reason}
   end
 end
